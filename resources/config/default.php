bc0276cf429e25f6ce3b77851164b8d6ae45fd9b
muatik <muatik@gmail.com>
2013-09-20 17:51:15 +0300

    adopts the framework silex and includes initial configurations

---
+++ resources/config/default.php
@@ -0,0 +1,56 @@
+<?php
+
+require ROOT."/vendor/autoload.php";
+
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app = new Silex\Application();
+
+$app['locale'] = 'en';
+
+/**
+ *  MongoDB Options
+ */
+$app['db.config.mongodb'] = array(
+	'host' => 'mongodb://localhost/',
+	'dbname' => 'logmon',
+	'user' => 'root',
+	'password' => ''
+);
+
+$app['logging.database'] = 'logmon';
+
+
+/**
+ * registering services
+ * */
+$app->register(new Silex\Provider\SessionServiceProvider(), array());
+
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+	'monolog.logfile' = ROOT.'/temp/'.date('Y:m:d').'.log',
+	'monolog.name' => 'logmon',
+	'monolog.hander' => $app->share(
+		function(Application $app) {
+			return new Monolog\Handler\MongoDBHandler(
+				$app['db.config.mongodb'],
+				$app['logging.database'] //database name
+			);
+		}
+	)
+));
+
+$app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
+	'http_cache.cache_dir' => ROOT.'/temp/http'
+));
+
+$app['db.mongo'] = $app->share(
+	function($app) {
+		return new Mongo($app['db.config.mongodb']);
+	}
+);
+
+?>
