ba5e28be0e3027bc291886b59ec7c49b1ebe720b
muatik <muatik@gmail.com>
2013-09-21 13:23:49 +0300

    separets configurations and app's bootstrap

--- resources/config/default.php
+++ resources/config/default.php
@@ -1,47 +1,73 @@
 <?php
 
-require ROOT."/vendor/autoload.php";
-
-use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$app = new Silex\Application();
-
 $app['locale'] = 'en';
+$app['resources_path'] = realpath(__DIR__.'/../resources/');
 
 /**
- *  MongoDB Options
+ * DATA STORAGE CONFIGURATIONS
+ * ------------------------------------------------------------------
+ * This section sets where the application will save its data such as projects,
+ * users' configurations, alarms etc.
  */
-$app['db.config.mongodb'] = array(
-	'host' => 'mongodb://localhost/',
-	'dbname' => 'logmon',
-	'user' => 'root',
+
+// If you use MySQL, configure and comment out the following set:
+$app['db.config.mysql'] = array(
+	'host' => 'localhost',
+	'database' => 'logmon',
+	'user' => '',
 	'password' => ''
 );
 
-$app['logging.database'] = 'logmon';
+// If you use MongoDB, configure and comment out the following set:
+$app['db.config.mongodb'] = array(
+	'host' => 'mongodb://localhost/',
+	'database' => 'logmon',
+	'auth' => false, // Does mongodb require authentication?
+	'user' => '',
+	'password' => ''
+);
+
+// If you use a plain text file, configure and comment out the following set:
+$app['db.config.file'] = array(
+	'path' => ROOT.'/resources/storage/'
+);
+
 
 
 /**
- * registering services
- * */
-$app->register(new Silex\Provider\SessionServiceProvider(), array());
+ * LOGGING CONFIGURATIONS
+ * ------------------------------------------------------------------
+ * Warning: This is about the applications itself loging, not projects.
+ */
 
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+// if you would like to save logs in a text file, use the following set:
+$app['logging.target'] = 'file';
+$app['logging.file.config'] = $app['db.config.file'];
+$app['logging.file.name'] = date('Y:m').'.log';
 
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-	'monolog.logfile' = ROOT.'/temp/'.date('Y:m:d').'.log',
-	'monolog.name' => 'logmon',
-	'monolog.hander' => $app->share(
-		function(Application $app) {
-			return new Monolog\Handler\MongoDBHandler(
-				$app['db.config.mongodb'],
-				$app['logging.database'] //database name
-			);
-		}
-	)
-));
+
+// if you would like to save logs in MongoDB, use the following set: 
+$app['logging.target'] = 'mongodb';
+$app['logging.mongodb.config'] = $app['db.config.mongodb'];
+// the database is optional. if specified, overrides db.config.mongodb.database
+$app['logging.database'] = 'logmong'; 
+$app['logging.collection'] = 'logs';
+
+
+// if you would like to save logs in MySQL, use the following set: 
+$app['logging.target'] = 'mysql';
+$app['logging.mysql.config'] = $app['db.config.mysql'];
+// the database is optional. if specified, overrides db.config.mysql.database
+$app['logging.database'] = 'logmong'; 
+$app['logging.table'] = 'logs';
+
+
+
+
+
+
+
+
 
 $app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
 	'http_cache.cache_dir' => ROOT.'/temp/http'
